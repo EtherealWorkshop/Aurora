89c12168acb855cbbba177ae46eaaa28d89ace6c
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-23 16:28:14 -0400

    uh

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -112,8 +112,11 @@ installcros() {
     read_center -d "Block ChromeOS and Kernel Updates? (Y/n): " block
     case $block in
         n|N) chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes || fail "Failed during chroot!" --fatal ;;
-        *) echo "Blocking Updates..."
-           chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes --minimal_copy || fail "Failed during chroot!" --fatal ;;
+        *) echo_c "Blocking Updates" GEEN_B | center
+           mv ./usr/sbin/chromeos-install.sh ./usr/sbin/chromeos-install.sh.bak
+           cp /usr/share/aurora/assets/chromeos-install.sh ./usr/sbin/chromeos-install.sh
+           chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes --minimal_copy || fail "Failed during chroot!" --fatal
+           mv ./usr/sbin/chromeos-install.sh.bak ./usr/sbin/chromeos-install.sh ;;
     esac # see, "case" spelled backwards is "esac", which is funny because until i've had my "case", i don't give "esac" about anything.
     local cros_dev="$(get_largest_cros_blockdev)"
     cgpt add -i 2 $cros_dev -P 15 -T 15 -S 1 -R 1 || echo -e "${YELLOW_B}Failed to set kernel priority! Continuing anyway${COLOR_RESET}"
