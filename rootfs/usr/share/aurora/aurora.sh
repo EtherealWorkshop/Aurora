8a4f97c8989892b2610bbc9f54120d7fdad53c0a
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-23 15:03:48 -0400

    nugget

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -110,6 +110,7 @@ installcros() {
         mount -n --bind "${d}" "./${d}"
         mount --make-slave "./${d}"
     done
+    debug_run nano usr/sbin/chromeos-install
     chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes || fail "Failed during chroot!" --fatal
     local cros_dev="$(get_largest_cros_blockdev)"
     cgpt add -i 2 $cros_dev -P 15 -T 15 -S 1 -R 1 || echo -e "${YELLOW_B}Failed to set kernel priority! Continuing anyway${COLOR_RESET}"
