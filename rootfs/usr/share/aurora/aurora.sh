a5636b73002b03a507e0f9fd77fb1e029b1e9818
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-23 16:35:52 -0400

    a

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -113,10 +113,9 @@ installcros() {
     case $block in
         n|N) chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes || fail "Failed during chroot!" --fatal ;;
         *) echo_c "Blocking Updates" GEEN_B | center
-           mv ./usr/sbin/chromeos-install.sh ./usr/sbin/chromeos-install.sh.bak
-           cp /usr/share/aurora/assets/chromeos-install.sh ./usr/sbin/chromeos-install.sh
-           chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes --minimal_copy || fail "Failed during chroot!" --fatal
-           mv ./usr/sbin/chromeos-install.sh.bak ./usr/sbin/chromeos-install.sh ;;
+           mount -n --bind /usr/share/aurora/assets/chromeos-install.sh ./usr/sbin/chromeos-install.sh
+           chroot ./ /usr/sbin/chromeos-install --payload_image="${loop}" --yes --minimal_copy || fail "Failed during chroot!" --fatal 
+           umount ./usr/sbin/chromeos-install.sh;;
     esac # see, "case" spelled backwards is "esac", which is funny because until i've had my "case", i don't give "esac" about anything.
     local cros_dev="$(get_largest_cros_blockdev)"
     cgpt add -i 2 $cros_dev -P 15 -T 15 -S 1 -R 1 || echo -e "${YELLOW_B}Failed to set kernel priority! Continuing anyway${COLOR_RESET}"
