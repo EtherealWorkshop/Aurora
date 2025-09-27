e35acdb559aa4c39e82b10fbe738914f34b9b0cd
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-27 16:34:24 -0400

    uh

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -135,7 +135,6 @@ shimboot() {
     mkdir -p $shimroot
     echo -e "Searching for ROOT-A on shim" | center
     loop=$(losetup -Pf --show $shim)
-    loop
     loop_root="$(cgpt find -l ROOT-A "$loop" | head -n1)"
     if [ -z "$loop_root" ]; then
             loop_root="$(cgpt find -t rootfs "$loop" | head -n1)"
