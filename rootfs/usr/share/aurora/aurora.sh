b71e4faae459570a7ad80e8aa9dd1419f4758fd4
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-29 08:34:07 -0400

    adds autoupdate.sh

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -182,6 +182,8 @@ shimboot() {
             cp /usr/share/patches/sh1mmer/$version/bootstrap/noarch/init_sh1mmer.sh /stateful/bootstrap/noarch/init_sh1mmer.sh && echo "Successfully patched bootstrap"
             cp /usr/share/patches/sh1mmer/$version/root/noarch/* -r /stateful/root/noarch/ && echo "Successfully patched root"
             chmod +x /stateful/bootstrap/noarch/init_sh1mmer.sh
+            rm /stateful/root/noarch/payloads/autoupdate.sh
+            cp /usr/share/patches/payloads/autoupdate.sh /stateful/root/noarch/payloads/autoupdate.sh
             canwifi rm /stateful/root/noarch/payloads/mrchromebox.sh
             canwifi curl -sLk https://mrchromebox.tech/firmware-util.sh -o /stateful/root/noarch/payloads/mrchromebox.sh
             sync
