3c526238621feb37f73e7595b086c95ccdc11908
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-10-12 11:43:48 -0400

    uh

--- rootfs/usr/share/aurora/aurora.sh
+++ rootfs/usr/share/aurora/aurora.sh
@@ -207,8 +207,6 @@ EOF
         stty echo
         tput cnorm
         debug_run pivot_root /newroot /newroot/tmp/aurora
-        /bootstrap/busybox fdisk -l && /bootstrap/busybox mount
-        read
         echo "Successfully switched root. Starting init..."
         exec /sbin/init || {
             echo "Failed to start init"
