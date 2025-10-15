15cdf14c31c38d776650ba44f3cdc1ca76195e83
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-10-15 18:02:56 -0400

    remove >/dev/null

--- auroraboot/opt/setup_auroraboot.sh
+++ auroraboot/opt/setup_auroraboot.sh
@@ -8,6 +8,6 @@ https://dl-cdn.alpinelinux.org/alpine/edge/community
 https://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
-apk add --no-cache e2fsprogs e2fsprogs-extra parted >/dev/null 2>&1
+apk add --no-cache e2fsprogs e2fsprogs-extra parted
 
 rm -rf /var/cache/apk/* || true
